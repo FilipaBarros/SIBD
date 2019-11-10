9d22414d85a5f2d9ca8b3d2e4d1bc2890fa911d8
Filipa Barros <up201305683@fe.up.pt>
2019-11-10 18:54:45 +0000

    Update SIBD_SQLite_DB_script.sql

--- SIBD_SQLite_DB_script.sql
+++ SIBD_SQLite_DB_script.sql
@@ -2,11 +2,12 @@
 DROP TABLE IF EXISTS UserPermissions;
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Systems;
+DROP TABLE IF EXISTS DevicesCategories;
 DROP TABLE IF EXISTS Devices;
 DROP TABLE IF EXISTS Categories;
-DROP TABLE IF EXISTS Components;
 DROP TABLE IF EXISTS Sensors;
 DROP TABLE IF EXISTS Actuators;
+DROP TABLE IF EXISTS Components;
 DROP TABLE IF EXISTS Locals;
 DROP TABLE IF EXISTS PermissionTypes;
 
@@ -40,14 +41,36 @@ CREATE TABLE PermissionTypes(
 CREATE TABLE UserPermissions(
     permid INTEGER NOT NULL PRIMARY KEY,
     userid INTEGER NOT NULL, 
-    systid INTEGER NOT NULL,
+    sysid INTEGER NOT NULL,
 	permtypeid INTEGER NOT NULL,
 	FOREIGN KEY (userid) REFERENCES Users(userid),
-    FOREIGN KEY (systid) REFERENCES Systems(systid),
+    FOREIGN KEY (sysid) REFERENCES Systems(sysid),
     FOREIGN KEY (permtypeid) REFERENCES PermissionTypes(permtypeid)
 );
 
 
+
+
+-- locals tables
+CREATE TABLE Locals(
+    locid INTEGER NOT NULL PRIMARY KEY,
+    country TEXT NOT NULL,
+    city TEXT NOT NULL,
+    zip TEXT NOT NULL,
+    street TEXT NOT NULL,
+    building TEXT NOT NULL,
+    floordesc TEXT NOT NULL,
+    room TEXT NOT NULL
+);
+
+
+
+-- Categories Tables 
+CREATE TABLE Categories(
+    catid INTEGER NOT NULL PRIMARY KEY,
+    catname TEXT NOT NULL,
+    catdescription TEXT NOT NULL
+);
 
 -- devices table
 CREATE TABLE Devices(
@@ -58,15 +81,21 @@ CREATE TABLE Devices(
     swversion TEXT NOT NULL,
     swartefact TEXT NOT NULL,
     ip TEXT NOT NULL,
-    stat TEXT  NOT NULL
+    stat TEXT  NOT NULL,
+    locid INTEGER NOT NULL,
+    FOREIGN KEY (locid) REFERENCES Locals(locid) 
 );
 
--- Categories Tables 
-CREATE TABLE Categories(
-    catid INTEGER NOT NULL PRIMARY KEY,
-    catname TEXT NOT NULL,
-    catdescription TEXT NOT NULL
+--devices categories
+CREATE TABLE DevicesCategories(
+    devcatid INTEGER NOT NULL PRIMARY KEY,
+    devid INTEGER NOT NULL,
+    catid INTEGER NOT NULL,
+    FOREIGN KEY (devid) REFERENCES Devices(devid),
+    FOREIGN KEY (catid) REFERENCES Categories(catid)
 );
+
+
 
 
 -- components table 
@@ -96,17 +125,6 @@ CREATE TABLE Actuators(
     compname TEXT NOT NULL,
     compcode INTEGER NOT NULL,
     stat TEXT  NOT NULL,
-	FOREIGN KEY (actid) REFERENCES Components(actid)
+	FOREIGN KEY (actid) REFERENCES Components(compid)
 );
 
--- locals tables
-CREATE TABLE Locals(
-    locid INTEGER NOT NULL PRIMARY KEY,
-    country TEXT NOT NULL,
-    city TEXT NOT NULL,
-    zip TEXT NOT NULL,
-    street TEXT NOT NULL,
-    building TEXT NOT NULL,
-    floordesc TEXT NOT NULL,
-    room TEXT NOT NULL
-);
