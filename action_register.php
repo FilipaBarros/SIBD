a13502d7c732d86f1d195f2fb0c161e20c402484
Hugo Rodrigues <34515264+rhugo97@users.noreply.github.com>
2019-11-17 19:20:17 +0000

    Update action_register.php

--- action_register.php
+++ action_register.php
@@ -1,22 +1,22 @@
 <?php
-    require_once('config/init');
+    require_once('config/init.php');
 
     $username=$_POST['username'];
     $password=$_POST['password'];
-    $email=$_POST['email'];
+    $email=$_POST['emailaddress'];
 
     function newUser($user,$pass,$mail){
         global $dataB;
-        $statement = $dataB->prepare('INSERT INTO Users VALUES(?,?,?)');
+        $statement = $dataB->prepare('INSERT INTO Users (username,passphrase,contact) VALUES (?,?,?)');
         $statement->execute(array($user,sha1($pass),$mail));
+        print_r($statement);
     }
 
-    try {
-        newUser($username, $password, $email);
-        header('Location: login_page.php');
-    }
-    catch(PDOException $e){
-        header('Location: register_page.php');
-    }
+    newUser($username, $password, $email);
+    header('Location: login_page.php');
+    
+   // catch(PDOException $e){
+   //     header('Location: register_page.php');
+    //}
 
 ?>
