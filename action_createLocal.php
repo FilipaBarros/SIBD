636f1db1bbac1fa7a543f0cd4075f96dfa1db8f4
Hugo Rodrigues <34515264+rhugo97@users.noreply.github.com>
2019-11-18 15:23:19 +0000

    insert local into database

---
+++ action_createLocal.php
@@ -0,0 +1,26 @@
+<?php
+    require_once('config/init.php');
+
+    $country=$_POST['country'];
+    $city=$_POST['city'];
+    $zip=$_POST['zip'];
+    $street=$_POST['street'];
+    $building=$_POST['building'];
+    $floor=$_POST['floor'];
+    $room=$_POST['room'];
+
+    function newLocal($country, $city, $zip, $street, $building, $floor, $room){
+        global $dataB;
+        $statement = $dataB->prepare('INSERT INTO Locals(country,city,zip,street,building,floordesc,room) VALUES (?,?,?,?,?,?,?)');
+        $statement->execute(array($country, $city,$zip,$street,$building,$floor,$room,$local));
+        print_r($statement);
+    }
+
+    newLocal($country, $city, $zip, $street, $building, $floor, $room);
+    header('Location: createLocal_page.php');
+    
+   // catch(PDOException $e){
+   //     header('Location: register_page.php');
+    //}
+
+?>
