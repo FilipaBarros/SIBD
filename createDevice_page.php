2d7921ecc25d46289a37bd7dac641fbc7abf93c9
Hugo Rodrigues <34515264+rhugo97@users.noreply.github.com>
2019-11-18 14:58:05 +0000

    paragraphs and format for ip addresses

--- createDevice_page.php
+++ createDevice_page.php
@@ -16,24 +16,24 @@
                     echo "<h2>".$title."</h2>";
                 ?>
                 <form action="action_createDevice.php" method="post">
-                    <input type="text" name="deviceName" placeholder="Device Name" required>
-                    <input type="text" name="manufacturer" placeholder="Manufacturer" required>
-                    <input type="text" name="description" placeholder="Description" required>
-                    <input type="text" name="swversion" placeholder="Software Version" required>
-                    <input type="text" name="swartefact" placeholder="Software Artefact" required>
-                    <input type="text" name="ip" placeholder="IP Address" required>
-                    <input type="text" name="status" placeholder="Status" required>
-                    <select name ="local" required>
+                    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Name: <input type="text" name="deviceName" placeholder="Device Name" required></p>
+                    <p>Manufacturer: <input type="text" name="manufacturer" placeholder="Manufacturer" required></p>
+                    <p>&nbsp&nbsp Description: <input type="text" name="description" placeholder="Description" required></p>
+                    <p>&nbsp&nbsp SW Version:<input type="text" name="swversion" placeholder="Software Version" required></p>
+                    <p>&nbspSW Artefact: <input type="text" name="swartefact" placeholder="Software Artefact" required></p>
+                    <p>&nbsp&nbsp&nbsp IP Address: <input type="text" name="ip" placeholder="IP Address" required pattern="^([0-9]{1,3}\.){3}[0-9]{1,3}$"></p>
+                    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspStatus: <input type="text" name="status" placeholder="Status" required></p>
+                    <p>Local ID: <select name ="local" required>
                         <?php
                             global $dataB;
-                            $queryLocal = "SELECT room FROM Locals";
+                            $queryLocal = "SELECT locid FROM Locals";
                             $result = $dataB->query($queryLocal);
                             while ($row = $result->fetch()){
-                                echo "<option value=\"local\">" . $row['room'] . "</option>";
+                                echo "<option value=\"local\">" . $row['locid'] . "</option>";
                             }
                             
                         ?>
-                    </select>
+                    </select></p>
                     <input type="submit" value="Create">
                 </form>
                 
