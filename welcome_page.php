cd2cb353973ac7b852e64d77953fad94e1ce3f15
Filipa Barros <up201305683@fe.up.pt>
2019-12-02 17:32:32 +0000

    grid

--- welcome_page.php
+++ welcome_page.php
@@ -1,30 +1,52 @@
 <?php include ('partials/header.php') ?>
 
-<h2>Locals</h2>
-<?php
-    $queryLocal = "SELECT COUNT(*) FROM Locals";
-    $result = $dataB->query($queryLocal);
-    $local_count = $result->fetchAll()[0];
-    echo $local_count[0];
-?>
-<br>
-<h2>Users</h2>
-<?php
-    $queryLocal = "SELECT COUNT(*) FROM Users";
-    $result = $dataB->query($queryLocal);
-    $device_count = $result->fetchAll()[0];
-    echo $device_count[0];
-?>
-<br>
-<h2>Devices</h2>
-<?php
-    global $dataB;
-    $queryLocal = "SELECT COUNT(*) FROM Devices";
-    $result = $dataB->query($queryLocal);
-    $device_count = $result->fetchAll()[0];
-    echo $device_count[0];
-?>
 
+<div class="wrapper">
+    
 
+    <div class="box Users">
+        <h2>Users</h2>
+        <?php
+            $queryLocal = "SELECT COUNT(*) FROM Users";
+            $result = $dataB->query($queryLocal);
+            $device_count = $result->fetchAll()[0];
+            echo $device_count[0];
+        ?>
+    </div>
+
+    <div class="box Locals">
+        <h2>Locals</h2>
+        <?php
+            $queryLocal = "SELECT COUNT(*) FROM Locals";
+            $result = $dataB->query($queryLocal);
+            $local_count = $result->fetchAll()[0];
+            echo $local_count[0];
+        ?>
+    </div>
+
+    <div class="box Systems">
+        <h2>Systems</h2>
+        <?php
+            $queryLocal = "SELECT COUNT(*) FROM Systems";
+            $result = $dataB->query($queryLocal);
+            $device_count = $result->fetchAll()[0];
+            echo $device_count[0];
+        ?>
+    </div>
+
+    <div class="box Devices">
+        <h2>Devices</h2>
+        <?php
+            global $dataB;
+            $queryLocal = "SELECT COUNT(*) FROM Devices";
+            $result = $dataB->query($queryLocal);
+            $device_count = $result->fetchAll()[0];
+            echo $device_count[0];
+        ?>
+    </div>
+    <div class = "box graph">Graph 1</div>
+    <div class = "box graph">Graph 2</div>
+    
+</div>
 
 <?php include ('partials/footer.php') ?>
