d3c18c0fa5e1ad2b78184533152d2ff8e3faad26
Hugo Rodrigues <34515264+rhugo97@users.noreply.github.com>
2019-12-06 03:23:47 +0000

    change permissions

---
+++ users/actions/action_change_permissions.php
@@ -0,0 +1,16 @@
+<?php
+    require_once('../../config/init.php');
+
+    $userId=$_POST['userId'];
+    $permission=$_POST['permission'];
+    $sysId=$_POST['sysId'];
+
+    global $dataB;
+    $statement = $dataB->prepare('UPDATE UserPermissions SET permtypeid=? WHERE userid=? AND sysid=?');
+    $statement->execute(array($permission,$userId,$sysId));
+    print_r($statement);
+
+    header('Location: ../users.php');
+
+
+?>
