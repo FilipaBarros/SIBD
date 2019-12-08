dbc3431dee98c59ecfb35919f6af03ed2ef7f3a1
Filipa Barros <up201305683@fe.up.pt>
2019-12-08 21:38:02 +0000

    more devices info udate 3

--- devices/details_device.php
+++ devices/details_device.php
@@ -3,6 +3,7 @@
 <?php
     $device_id = $_GET['id']; 
     $allcompcodes = array();
+    $localid = 0;
     function get_table_devices_categories($device_id)
     {
         global $dataB;
@@ -46,24 +47,62 @@
         $statement = $dataB->prepare("SELECT Systems.sysid, Systems.sysdescription, Systems.category, Systems.functions 
                                       from Systems JOIN Devices on Devices.sysid = Systems.sysid 
                                       WHERE Devices.devid = ?;");
-        $statement->execute(array($compid));
+        $statement->execute(array($device_id));
         $systems = $statement->fetchAll();
         return $systems;
     }
     //yep, again for info and stuff
-    function get_table_device(){ 
-
+    function get_table_device($device_id){ 
+        global $dataB;
+        $statement = $dataB->prepare("SELECT * From Devices WHERE devid = ?");
+        $statement->execute(array($device_id));
+        $device = $statement->fetchAll();
+        global $localid;
+        $localid = $device[0]["locid"];
+        return $device;
     }
     function get_table_location(){
-        
+        global $dataB;
+        global $localid;
+        $statement = $dataB->prepare("SELECT * From Locals WHERE locid = ?");
+        $statement->execute(array($localid));
+        $local = $statement->fetchAll();
+        return $local;
     }
 
 
 ?>
 <h2>Device Info</h2>
 <?php
+    //device info again
+    $res4 = get_table_device($device_id);
+    echo "<br>";
+    echo "<table>";
+    echo " <tr> ";
+    echo "<th>  #           </th>";
+    echo "<th>  DevName     </th>";
+    echo "<th>  Manufacturer</th>";
+    echo "<th>  Description </th>";
+    echo "<th>  SWVersion   </th>";
+    echo "<th>  SWArtefact  </th>";
+    echo "<th>  IP          </th>";
+    echo "<th>  Status      </th>";
+    echo "<th>  Local ID    </th>";
+    echo "<th>  System ID   </th>";
+    echo " </tr> ";
+    foreach ($res4 as $row) {
+        echo "<tr>";
+        for ($j = 0; $j < 10; $j++) { // we're expecting 10 attributes
+            echo "<td> " . $row[$j] . " </td>"; // gives the current item of the current attribute
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+
+
     //categories
     $res = get_table_devices_categories($device_id);
+    echo "<br>";
     echo "<h3>Associated Categories</h3>";
     echo "<table>";
     echo " <tr> ";
@@ -71,7 +110,6 @@
     echo "<th>Category Name       </th>";
     echo "<th>Category Description</th>";
     echo " </tr> ";
-    echo " <br> ";
     foreach($res as $row){
         echo "<tr>";
         //print_r($row);
@@ -79,12 +117,12 @@
         echo "<td> " . $row['catname'] . " </td>";
         echo "<td> " . $row['catdescription'] . " </td>";
     }
-    echo "<br>";
     echo "</tr>";
     echo "</table>";
 
     //components then sensors and actuators
     $res2 = get_table_components($device_id);
+    echo "<br>";
     echo "<h3>Associated Components</h3>";
     //foreach($res2 as $row){
     //    print_r($row);
@@ -100,6 +138,7 @@
 
     //sensors
     //print_r($allsensors);
+    echo "<br>";
     echo "<h4>Sensors</h4>";
     echo "<table>";
     echo "<tr>";
@@ -110,7 +149,6 @@
     echo "<th> Component Code      </th>";
     echo "<th> Status              </th>";
     echo " </tr> ";
-    echo " <br> ";
     foreach($allsensors as $res){
         for ($j = 0; $j < count($res); $j++) { 
             echo "<tr>";
@@ -128,6 +166,7 @@
     
     //actuators
     //print_r($allactuators);
+    echo "<br>";
     echo "<h4>Actuators</h4>";
     echo "<table>";
     echo "<tr>";
@@ -137,7 +176,6 @@
     echo "<th> Component Code      </th>";
     echo "<th> Status              </th>";
     echo " </tr> ";
-    echo " <br> ";
     foreach($allactuators as $res){
         for ($j = 0; $j < count($res); $j++) { 
             echo "<tr>";
@@ -152,11 +190,54 @@
     echo "</tr>";
     echo "</table>";
 
+    //systems 
+    $res3 = get_table_systems($device_id);
+    //print_r($res3);
+    echo "<h3>Systems</h3>";
+    echo "<table>";
+    echo " <tr> ";
+    echo "<th> #                  </th>";
+    echo "<th>Description         </th>";
+    echo "<th>Category            </th>";
+    echo "<th>Function            </th>";
+    echo " </tr> ";
+    foreach($res3 as $row){
+        echo "<tr>";
+        //print_r($row3);
+        echo "<td> " . $row['sysid'] . " </td>";
+        echo "<td> " . $row['sysdescription'] . " </td>";
+        echo "<td> " . $row['category'] . " </td>";
+        echo "<td> " . $row['functions'] . " </td>";
+    }
+    echo "<br>";
+    echo "</tr>";
+    echo "</table>";
+
+    //location info 
+    $res5 = get_table_location();
+    echo "<h3>Location Info</h3>";
+    echo "<table>";
+    echo " <tr> ";
+    echo "<th> # </th>";
+    echo "<th> Country </th>";
+    echo "<th> City </th>";
+    echo "<th> Zip </th>";
+    echo "<th> Street </th>";
+    echo "<th> Building </th>";
+    echo "<th> Floor </th>";
+    echo "<th> Room </th>";
+    echo " </tr> ";
+    foreach ($res5 as $row) {
+        echo "<tr class='listelement'>";
+        for ($j = 0; $j < 8; $j++) { // we're expecting eight attributes
+            echo "<td> " . $row[$j] . " </td>"; // gives the current item of the current attribute
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+
+
+
 
 ?>
 <?php include('../partials/footer.php'); ?> 
-
-
-
-
-
