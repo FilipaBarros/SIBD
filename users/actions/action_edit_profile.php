fe4a42b0f28d4117fa0edeebf3aab43272531141
Hugo Rodrigues <34515264+rhugo97@users.noreply.github.com>
2019-12-09 22:37:15 +0000

    check database for username

--- users/actions/action_edit_profile.php
+++ users/actions/action_edit_profile.php
@@ -8,8 +8,19 @@
     $pass=$_POST['password'];
 
     global $dataB;
-    $statement = $dataB->prepare('UPDATE Users SET username=?, passphrase=?, contact=? WHERE userid=?');
-    $statement->execute(array($username,sha1($pass),$contact,$id));
+
+    $statement=$dataB->prepare('SELECT COUNT(*) AS num FROM Users WHERE username=?');
+    $statement->execute(array($username));
+    $row=$statement->fetch();
+    print_r($statement);
+
+    if($row['num']==0){
+        $statement = $dataB->prepare('UPDATE Users SET username=?, passphrase=?, contact=? WHERE userid=?');
+        $statement->execute(array($username,sha1($pass),$contact,$id));
+    }
+    else{
+        header('Location: ../edit_profile.php');
+    }
 
     header('Location: ../edit_profile.php');
 
