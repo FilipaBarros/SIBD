3bf8fdf949c7d7f10ccd6cda0e3591c8ba056578
Hugo Rodrigues <34515264+rhugo97@users.noreply.github.com>
2019-12-09 19:20:21 +0000

    added username verification

--- users/actions/action_register.php
+++ users/actions/action_register.php
@@ -7,24 +7,36 @@
 
     function newUser($user,$pass,$mail){
         global $dataB;
-        $statement = $dataB->prepare('INSERT INTO Users (username,passphrase,contact) VALUES (?,?,?)');
-        $statement->execute(array($user,sha1($pass),$mail));
-        
-        $newUserID=$dataB->lastInsertID();
-        
-        $statement = $dataB->query("SELECT COUNT(*) FROM Systems");
-        $numSystems = $statement->fetchColumn();
+        $statement=$dataB->prepare('SELECT COUNT(*) AS num FROM Users WHERE username=?');
+        $statement->execute(array($user));
+        $row=$statement->fetch();
+        print_r($statement);
 
-        for($i=1;$i<=$numSystems;$i++){
-            $statement = $dataB->prepare("INSERT INTO UserPermissions(userid,sysid,permtypeid) VALUES (?,?,?)");
-            $statement->execute(array($newUserID,$i,2));
-            print_r($statement);
+        if($row['num']==0){
+            $statement = $dataB->prepare('INSERT INTO Users (username,passphrase,contact) VALUES (?,?,?)');
+            $statement->execute(array($user,sha1($pass),$mail));
+        
+            $newUserID=$dataB->lastInsertID();
+        
+            $statement = $dataB->query("SELECT COUNT(*) FROM Systems");
+            $numSystems = $statement->fetchColumn();
+
+            for($i=1;$i<=$numSystems;$i++){
+                $statement = $dataB->prepare("INSERT INTO UserPermissions(userid,sysid,permtypeid) VALUES (?,?,?)");
+                $statement->execute(array($newUserID,$i,2));
+                print_r($statement);
+            }
+        }
+        else{
+            header('Location: ../../index.php');
+            
         }
 
+        
     }
 
     newUser($username, $password, $email);
-    header('Location: ../login_page.php');
+    header('Location: ../../index.php');
     
    // catch(PDOException $e){
    //     header('Location: register_page.php');
