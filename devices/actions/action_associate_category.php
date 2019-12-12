7f166eef4ba2c37dab81d87399da32925e31ba79
Hugo Rodrigues <34515264+rhugo97@users.noreply.github.com>
2019-12-12 19:53:45 +0000

    fixed actions

--- devices/actions/action_associate_category.php
+++ devices/actions/action_associate_category.php
@@ -3,14 +3,14 @@
     require_once('../../config/init.php');
 
     $category=$_GET['category'];
-    $devId=$_GET['devId'];
+    $devId=$_GET['id'];
 
     global $dataB;
     $statement=$dataB->prepare('INSERT INTO DevicesCategories(devid,catid) VALUES (?,?)');
     $statement->execute(array($devId,$category));
     print_r($statement);
 
-    header('Location: ../associate_category.php?devId='.$devId);
+    header('Location: ../associate_category.php?id='.$devId);
 
 
 
